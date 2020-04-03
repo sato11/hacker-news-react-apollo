b48724bfb9dfe174b2160d06a5030c9c68a22ec3
Sato Junichi <22004610+sato11@users.noreply.github.com>
2020-04-03 15:52:45 +0900

    update the number of votes after voting

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -39,13 +39,27 @@ class LinkList extends Component {
           return (
             <div>
               {linksToRender.map((link, index) => (
-                <Link key={link.id} link={link} index={index} />
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
               ))}
             </div>
           )
         }}
       </Query>
     );
+  };
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    const votedLink = data.feed.links.find(link => link.id == linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
   };
 };
 
