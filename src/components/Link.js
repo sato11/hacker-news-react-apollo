6eb21b9dff0a5d1b9c4c21866411b17bab506d7e
Sato Junichi <22004610+sato11@users.noreply.github.com>
2020-04-03 15:44:54 +0900

    add mutation to give upvote to a post

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import { AUTH_TOKEN } from '../constants';
 import { timeDifferenceForDate } from '../utils';
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
 
 class Link extends Component {
   render() {
@@ -11,9 +33,13 @@ class Link extends Component {
         <div className="flex items-center">
           <span className="gray">{this.props.index + 1}.</span>
           {authToken && (
-            <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
-              ▲
-            </div>
+            <Mutation mutation={VOTE_MUTATION} variables={{ linkId: this.props.link.id }}>
+              {voteMutation => (
+                <div className="ml1 gray f11" onClick={voteMutation}>
+                  ▲
+                </div>
+              )}
+            </Mutation>
           )}
         </div>
         <div className="ml1">
