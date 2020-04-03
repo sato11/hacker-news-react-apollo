413ebc843cd89499a0393f942d40dd525673e0bd
Sato Junichi <22004610+sato11@users.noreply.github.com>
2020-04-03 12:00:26 +0900

    add router to enable navigation between paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
+import Header from './Header';
+import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 
 function App() {
   return (
-    <CreateLink />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
