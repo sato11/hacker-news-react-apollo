340e672aa2730218328fb12141f283cfed897596
Sato Junichi <22004610+sato11@users.noreply.github.com>
2020-04-03 11:46:03 +0900

    add react-apollo component to execute mutation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 function App() {
   return (
-    <LinkList />
+    <CreateLink />
   );
 }
 
